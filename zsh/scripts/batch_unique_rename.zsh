e8be832cad805c3f84e22240038cf95fbd35895f
zeric <zeric0212@hotmail.com>
2022-04-26 00:53:58 +0800

    script restructure

--- zsh/scripts/batch_unique_rename.zsh
+++ zsh/scripts/batch_unique_rename.zsh
@@ -3,8 +3,10 @@
 # Removing the unicode zero-width space \u200b
 # sed "s/$(echo -ne '\u200b')//g" *.csv
 
+# create a csv file with oldname as column A, newname as column B
+
 cat *.csv | while IFS=, read -r oldname newname; do
 
-	rename -s $oldname $newname Images/*
+		rename -s $oldname $newname Images/*
 
-done
+	    done

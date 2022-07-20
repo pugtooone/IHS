4ac859d63008115842266b49e2c8b6ca0e83bb75
zeric <zeric0212@hotmail.com>
2022-07-21 00:52:34 +0800

    add get_doc_list() and internal _doc_glob() for constructing a list of documents

--- python/package/pyhs/JobDir/Doc.py
+++ python/package/pyhs/JobDir/Doc.py
@@ -7,4 +7,28 @@ class Doc:
         Parameter: Path obj of JobDir
         """
         self.docDir = directory / 'Documents'
-        self.docList = os.listdir(self.docDir)
+        self.docListDir = os.listdir(self.docDir)
+
+    # globbing pattern for counting the documents, which is universal to all instances: {'pattern to search for': 'document'}
+    docPattern = {'*Post-production*': 'post-production guideline', '*Shoot Brief*': 'shoot brief', '*Retouch Note*': 'retouch note', '*Swatch*': 'swatches', '*Overlay*': 'overlay', '*Feedback*': 'feedback.pdf'}
+
+    def get_doc_list(self):
+        """
+        create a dictionary that store the instances of documents
+        """
+        self.docList = []
+        # append the documents(values) to the docList(list), if pattern(keys) present
+        for pattern, doc in self.docPattern.items():
+            if self._doc_glob(pattern):
+                self.docList.append(doc)
+        return self.docList
+
+    def _doc_glob(self, pattern):
+        """
+        internal pattern search method for get_doc_list()
+        could change to re for case-insensitive matching
+        """
+        try:
+            return next(self.docDir.glob(pattern))
+        except StopIteration:
+            return None

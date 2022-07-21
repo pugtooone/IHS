6fb765e61ba6f62f898ced34eed5da3db4ec4a68
zeric <zeric0212@hotmail.com>
2022-07-22 01:00:16 +0800

    return email template

--- python/package/pyhs/JobDir/ToSend.py
+++ python/package/pyhs/JobDir/ToSend.py
@@ -9,3 +9,4 @@ class ToSend(JobDir):
         self.email = f'Hi!\n\nPlease note that {self.jobName} is being uploaded to the server, including {self.imgNum} images along with {self.docItems}. Let me know if there is any question. Thanks!\n\n'
         pyperclip.copy(self.email)
         print('email template copied')
+        return self.email

9a6658a0dfe456e148a14f2ba4f65ce02a24c9fd
zeric <zeric0212@hotmail.com>
2022-07-21 18:08:26 +0800

    rearrange attributes to be called upon initialization

--- python/package/pyhs/JobDir/JobDir.py
+++ python/package/pyhs/JobDir/JobDir.py
@@ -10,14 +10,15 @@ class JobDir:
         self.jobDir = directory
         self.jobName = directory.name
         self.imgObj = Img(self.jobDir)
+        self.imgNum = self.imgObj.get_img_num()
         self.docObj = Doc(self.jobDir)
+        self.docList = self.docObj.get_doc_list()
+        self.docItems = self.docObj.get_doc_items()
 
     def get_doc(self):
-        self.docList = self.docObj.get_doc_list()
         return self.docList
 
     def get_img_no(self):
-        self.imgNum = self.imgObj.get_img_num()
         return self.imgNum
 
     def check_img_spec(self):

28c0352ef7297b03231c5cd998032b16978d5e17
zeric <zeric0212@hotmail.com>
2022-08-05 12:13:53 +0800

    create javascript/

---
+++ javascript/Photoshop/hideLayers.jsx
@@ -0,0 +1,31 @@
+#target photoshop;
+
+app.bringToFront();
+function main(){
+    hideLayers(activeDocument);
+}
+
+
+function hideLayers(ref){
+    var len = ref.layers.length;
+
+    for(var i = 0 ; i < len ; i++){
+        if(ref.layers[i].kind == 'LayerKind.SOLIDFILL'){
+            ref.layers[i].visible = false;
+            for (var j = i ; j < len  ; j++){
+                ref.layers[j].visible = false;
+                return;
+            }
+        }
+    }
+
+}
+
+
+/*renameLayers = (ref) => {
+    var layer = ref.layers[2];
+    alert(layer.typename);
+
+}*/
+
+main();

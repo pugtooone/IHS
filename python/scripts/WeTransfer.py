f92fd38166cc6f474754a6e51a5c20ae3aface9f
zeric <zeric0212@hotmail.com>
2022-08-30 18:15:26 +0800

    working on uploading files through WeTransfer

--- python/scripts/WeTransfer.py
+++ python/scripts/WeTransfer.py
@@ -1,38 +1,38 @@
-#! python3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pyinputplus as pyin
-# import time
+import os
 
-ac = 'zeric.chan@iheartstudios.com'
-pw = pyin.inputPassword('Enter Your Password: ')
+ac = os.environ.get("WET_AC")
+pw = os.environ.get("WET_PW")
 
 try:
     driver = webdriver.Chrome()
-    driver.get('https://mail.google.com/mail/u/0/#inbox')
+    driver.get('https://wetransfer.com')
 
-    acInput = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id ="identifierId"]')))
+    loginButt = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="navigation.login"]')))
+    loginButt.click()
+
+    acInput = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, '1-email')))
     acInput.send_keys(ac)
-    acInput.send_keys(Keys.RETURN)
 
-    pwInput = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')))
+    pwInput = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, 'password')))
     pwInput.send_keys(pw)
     pwInput.send_keys(Keys.RETURN)
 
     print('Login Success!')
 
-    composeButt = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div')))
-    composeButt.click()
+    cookieButt = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/div[2]/div[1]/div[3]/div[4]/button[1]')))
+    cookieButt.click()
+    # cookieButt = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/div[2]/div[1]/div[3]/div[4]/button[1]')))
+    # cookieButt.send_keys(Keys.RETURN)
 
-
-    """emailSubInput = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id=":q3"]')))
-    emailSubInput.send_keys('job')
-
-    emailMsgInput =WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id=":r9"]')))
-    emailMsgInput.send_keys('Hi,\nMessage Here\n')"""
+    # agreeButt = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[3]/div/div[2]/button')))
+    # agreeButt.click()
+    # composeButt = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div')))
+    # composeButt.click()
 
 except:
     print('Login Failed!')

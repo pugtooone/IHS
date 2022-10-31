7f5ad16789b1f8e5dd9e9d5974a5a2bade1bbfda
zeric <zeric0212@hotmail.com>
2022-10-31 18:17:53 +0800

    introduct select_text_extract(), to extract text with drawing selection

--- python/scripts/pdf_extract.py
+++ python/scripts/pdf_extract.py
@@ -47,19 +47,63 @@ def text_extract():
     pyperclip.copy(text)
     print('Text copied')
 
+def make_text(words):
+    """Return textstring output of get_text("words").
+    Word items are sorted for reading sequence left to right,
+    top to bottom.
+    """
+    line_dict = {}  # key: vertical coordinate, value: list of words
+    words.sort(key=lambda w: w[0])  # sort by horizontal coordinate
+    for w in words:  # fill the line dictionary
+        y1 = round(w[3], 1)  # bottom of a word: don't be too picky!
+        word = w[4]  # the text of the word
+        line = line_dict.get(y1, [])  # read current line content
+        line.append(word)  # append new word
+        line_dict[y1] = line  # write back to dict
+    lines = list(line_dict.items())
+    lines.sort()  # sort vertically
+    return "\n".join([" ".join(line[1]) for line in lines])
+
+def select_text_extract():
+    file = Path(askopenfilename())
+    pdf = fitz.open(file)
+
+    text = ""
+    drawingCount = 0
+
+    for pageNo in range(len(pdf)):
+        page = pdf[pageNo]
+
+        drawings = page.get_drawings()
+        for i in range(len(drawings)):
+            drawingCount += 1
+            rect = drawings[i]['rect']
+            # coorList = list(coor)
+            # text += page.get_textbox(coorList)
+
+            words = page.get_text("words")
+            mywords = [w for w in words if fitz.Rect(w[:4]).intersects(rect)]
+            filename = make_text(mywords)
+            if filename != '':
+                text += f'\r {filename}'
+
+    pyperclip.copy(text)
+    print('Text copied')
+    print(f'Total Selection: {drawingCount}')
+
 def main():
     while True:
-        print('Extract [Text] or [Images] from PDF?')
+        print('Extract [All Text] or [Selected Text] or [Images] from PDF?')
         answer = input()
-        try:
-            if answer.lower() == 'images':
-                img_extract()
-                sys.exit()
-            elif answer.lower() == 'text':
-                text_extract()
-                sys.exit()
-        except fitz.fitz.FileDataError:
-            print('No PDF selected')
+        if answer.lower() == 'images':
+            img_extract()
+            sys.exit()
+        elif answer.lower() == 'all text':
+            text_extract()
+            sys.exit()
+        elif answer.lower() == 'selected text':
+            select_text_extract()
+            sys.exit()
 
 if __name__ == "__main__":
     main()

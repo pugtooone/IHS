390cb5c5cea985e5659d2fd0b66f74059e606e5c
zeric <zeric0212@hotmail.com>
2023-03-17 18:43:55 +0800

    update

--- GoogleAppScript/autofillDates.js
+++ GoogleAppScript/autofillDates.js
@@ -10,33 +10,44 @@ function onEdit(e) {
   const reqDateCol = reqDateCell.getColumn();
   const deliDateCell = ss.getRange("Amendment Tracker!K2");
   const deliDateCol = deliDateCell.getColumn();
-  const shipBackDateCell = ss.getRange("Shot List!Q3");
+  const shipBackDateCell = ss.getRange("Shot List!Q2");
   const shipBackDateCol = shipBackDateCell.getColumn();
 
 //Amendment Tracker Autofill Dates
-  if (eColumn == reqDateCol + 3  && eValue !== "") {
+  if (ss.getSheetName() === "Amendment Tracker") {
 
-    const cell = e.range.offset(0, -3);
-    if (cell.getValue() === "" && cell.getRow() > 2 && reqDateCell.getValue() === "Request Date" && cell.getColumn() === reqDateCol) {
-     cell.setValue(new Date());
-    }
+    if (eColumn == reqDateCol + 3  && eValue !== "") {
 
-  } else if (eColumn == deliDateCol - 1  && eValue !== "") {
+      const cell = e.range.offset(0, -3);
+      
+      if (cell.getValue() === "" && cell.getRow() > 2 && reqDateCell.getValue() === "Request Date" && cell.getColumn() === reqDateCol) {
+        cell.setValue(new Date());
+      }
 
-    const cell = e.range.offset(0, 1);
-    if (cell.getValue() === "" && cell.getRow() > 2 && deliDateCell.getValue() === "Delivery Date" && cell.getColumn() === deliDateCol) {
-     cell.setValue(new Date());
-    }
+    } else if (eColumn == deliDateCol - 1  && eValue !== "") {
+
+      const cell = e.range.offset(0, 1);
+
+      if (cell.getValue() === "" && cell.getRow() > 2 && deliDateCell.getValue() === "Delivery Date" && cell.getColumn() === deliDateCol) {
+        cell.setValue(new Date());
+      }
+
+    };
 
   };
 
 //Shotlist Autofill Ship Back Date
-  if (eColumn == shipBackDateCol - 1  && eValue === "Shipped back") {
+  if (ss.getSheetName() === "Shot List") {
 
-    const cell = e.range.offset(0, 1);
-    if (cell.getValue() === "" && cell.getRow() > 3 && shipBackDateCell.getValue() === "Ship Back Date" && cell.getColumn() === shipBackDateCol) {
-     cell.setValue(new Date());
+    if (eColumn == shipBackDateCol - 1  && eValue === "Shipped back") {
+
+      const cell = e.range.offset(0, 1);
+
+      if (cell.getValue() === "" && cell.getRow() > 2 && shipBackDateCell.getValue() === "Ship Back Date" && cell.getColumn() === shipBackDateCol) {
+       cell.setValue(new Date());
+      };
     };
+
   };
 
 }

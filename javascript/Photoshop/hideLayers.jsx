7d15c271833d37ce104cfa7587f5cf2669129c64
BlitzQuit <102532124+BlitzQuit@users.noreply.github.com>
2023-05-09 15:28:01 +0800

    fixed bugs

--- javascript/Photoshop/hideLayers.jsx
+++ javascript/Photoshop/hideLayers.jsx
@@ -9,13 +9,13 @@ function main(){
 function hideLayers(ref){
     var len = ref.layers.length;
 
-    for(var i = 0 ; i < len ; i++){
+    for(var i = len-1 ; i > 0 ; i--){
         if(ref.layers[i].kind == 'LayerKind.SOLIDFILL'){
             ref.layers[i].visible = false;
             for (var j = i ; j < len  ; j++){
                 ref.layers[j].visible = false;
             }
-
+            break;
         }
     }
 

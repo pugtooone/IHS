af3c34b6cc201d5a84bfce4433b509b414666eaa
zeric <zeric0212@hotmail.com>
2023-06-14 15:58:41 +0800

    fix bug that auto deletes "Amendment" header; matchEntireCase for createTextFinder for realExtRetSKU

--- GoogleAppScript/autofillExtRet_Arena.js
+++ GoogleAppScript/autofillExtRet_Arena.js
@@ -4,7 +4,7 @@ function onOpen(e) {
   const tracker = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Amendment Tracker");
 
   //construct a product list with extensive retouch
-  const trackerSKUCol = tracker.createTextFinder('SKU Name').findNext().getColumn(); //get the column of "SKU Name"
+  const trackerSKUCol = tracker.createTextFinder('SKU Name').matchEntireCell(true).findNext().getColumn(); //get the column of "SKU Name"
   const extRetList = tracker.createTextFinder('EXTENSIVE RETOUCH').findAll(); //return ranges of all "EXTENSIVE RETOUCH", which later used for getting the row
   const extRetSKUList = [];
   for (let i = 0; i < extRetList.length; i++) {
@@ -15,18 +15,19 @@ function onOpen(e) {
   const realExtRetSKUListPP = JSON.stringify(realExtRetSKUList, null, 2);
   Logger.log('Products with Ext Ret:\n' + realExtRetSKUListPP);
 
-  const shotlistSKUCol = shotList.createTextFinder("SKU").findNext().getColumn();
-  const amendCol = shotList.createTextFinder('Amendment').findNext().getColumn();
+  const shotlistSKUCol = shotList.createTextFinder("SKU").matchEntireCell(true).findNext().getColumn();
+  const amendRow = shotList.createTextFinder('Amendment').matchEntireCell(true).findNext().getRow();
+  const amendCol = shotList.createTextFinder('Amendment').matchEntireCell(true).findNext().getColumn();
   const shotListLastRow = shotList.getLastRow();
 
-  const rangeToWrite = shotList.getRange(3, amendCol, shotListLastRow - 2);
+  const rangeToWrite = shotList.getRange(amendRow + 1, amendCol, shotListLastRow - amendRow + 1);
   rangeToWrite.clear({contentsOnly: true});
   Logger.log('Cleaning cells: ' + rangeToWrite.getA1Notation());
 
   for (let i = 0; i < realExtRetSKUList.length; i++) {
-    const rowToWrite = shotList.createTextFinder(realExtRetSKUList[i]).findNext().getRow();
+    const rowToWrite = shotList.createTextFinder(realExtRetSKUList[i]).matchEntireCell(true).findNext().getRow();
     const cellToWrite = shotList.getRange(rowToWrite, amendCol);
     cellToWrite.setValue("Extensive Retouch");
-    Logger.log('Writing to: ' + realExtRetSKUList[i] + '--' + cellToWrite.getA1Notation());
+    Logger.log('Writing to: ' + realExtRetSKUList[i] + ' --- ' + cellToWrite.getA1Notation());
   };
 };
